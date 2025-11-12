4356a71ed2c2d89dfc4cd943a70729ed39efe703
anyk3N <vuk0lov@yandex.by>
2025-11-12 23:42:06 +0300

    feat: add routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { GlobalStyles } from 'styles/GlobalStyles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import { ROUTES } from './constants/routes';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            {ROUTES.map(route => (
+              <Route key={route.path} path={route.path} element={route.element} />
+            ))}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/constants/routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CategoryPage from '../pages/CategoryPage';
+import NotFoundPage from '../pages/NotFoundPage';
+import ImagesPage from '../pages/ImagesPage';
+import FavouritesPage from '../pages/FavouritesPage';
+
+export const ROUTES = [
+  { path: '/', element: <CategoryPage /> },
+  { path: '/images', element: <ImagesPage /> },
+  { path: '/images/:category', element: <ImagesPage /> },
+  { path: '/favourites', element: <FavouritesPage /> },
+  { path: '*', element: <NotFoundPage /> },
+];
