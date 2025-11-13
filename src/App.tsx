0d0e8c751084658044daefac6fec2ba360783a3e
anyk3N <vuk0lov@yandex.by>
2025-11-13 23:06:30 +0300

    style(layout): adjust global wrapper styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import { GlobalStyles } from 'styles/GlobalStyles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import { ROUTES } from './constants/routes';
+import { Wrapper } from './styles/Wrapper';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            {ROUTES.map(route => (
-              <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Wrapper>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              {ROUTES.map(route => (
+                <Route key={route.path} path={route.path} element={route.element} />
+              ))}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Wrapper>
     </>
   );
 }
