552bf6bdd3b59fb21a19060aee401c7130c8b6b3
anyk3N <vuk0lov@yandex.by>
2025-11-18 17:33:35 +0300

    chore(app): integrate ErrorBoundary in main entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from 'App';
+import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
+import { ErrorFallback } from 'components/ErrorBoundary/ErrorFallback';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorFallback />}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
