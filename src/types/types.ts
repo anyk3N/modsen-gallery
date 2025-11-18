5bb5c9d3d7791d167d329bc6d9d232944fd1f158
anyk3N <vuk0lov@yandex.by>
2025-11-18 17:49:42 +0300

    refactor(types): add new props and interfaces for better type safety

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import React, { ReactNode } from 'react';
+
 export interface UnsplashPhoto {
   id: string;
   urls: {
@@ -21,6 +23,9 @@ interface BaseCardProps {
 
 export interface ImageCardProps extends BaseCardProps {
   title: string;
+  onClick: () => void;
+  favClick: () => void;
+  isActive: boolean;
 }
 
 export interface CategoryCardProps extends BaseCardProps {
@@ -39,3 +44,46 @@ export interface SelectorProps {
 export type ImagesListProps = {
   searchQuery: string;
 };
+
+export interface PhotoModalProps {
+  photos: UnsplashPhoto[];
+  currentIndex: number | null;
+  onClose: () => void;
+  onPrev: () => void;
+  onNext: () => void;
+}
+
+export type FavouritesContextType = {
+  favourites: UnsplashPhoto[];
+  toggleFavourite: (photo: UnsplashPhoto) => void;
+  isFavourite: (id: string) => boolean;
+};
+
+export interface FavouritesProviderProps {
+  children: ReactNode;
+}
+
+export type NavButtonProps = {
+  Icon?: React.ComponentType<{ color: string; fill: string }>;
+  title: string;
+  to: string;
+  isActive?: boolean;
+};
+
+export interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+export interface ExtendedNavButtonProps extends NavButtonProps {
+  $mobile?: boolean;
+  onClick?: () => void;
+}
+
+export interface IconsProps {
+  color?: string;
+  isActive?: boolean;
+  fill?: string;
+  onClick?: () => void;
+}
