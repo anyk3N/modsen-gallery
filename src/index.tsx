2228133a77ca326847e5bbd0de65618fa1985cd9
anyk3N <vuk0lov@yandex.by>
2025-11-24 00:34:53 +0300

    refactor: move App to own folder and update import

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import App from 'App';
+import App from 'components/App/App';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import { ErrorFallback } from 'components/ErrorBoundary/ErrorFallback';
 import React from 'react';
