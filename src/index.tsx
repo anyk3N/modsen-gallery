fa46a79bbbf1cd8943a8f06cb864c336cccaec04
anyk3N <vuk0lov@yandex.by>
2025-11-25 02:14:49 +0300

    chore(theme): wrap project with ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,17 @@ import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import { ErrorFallback } from 'components/ErrorBoundary/ErrorFallback';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from './styles/Variables';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ErrorBoundary fallback={<ErrorFallback />}>
-      <App />
-    </ErrorBoundary>
+    <ThemeProvider theme={theme}>
+      <ErrorBoundary fallback={<ErrorFallback />}>
+        <App />
+      </ErrorBoundary>
+    </ThemeProvider>
   </React.StrictMode>,
 );
